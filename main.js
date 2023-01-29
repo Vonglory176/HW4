cefcc62a7b9d8b704b27f1f0e587bf7089829cee
aecameron <aec.cameron@gmail.com>
2023-01-29 14:56:12 -0800

    added try catch block around most burned function, added calories display to most burned, removed extra i parameter in showAllButtonClick function

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function radiocheck() {
     return new Promise((resolve, reject) => {
         document.querySelectorAll('input[name="exerPick"]').forEach(i => { //Iterating all input elements with name 'exerPick'
             if (i.checked) resolve(i.id) //Returns ID of checked
-        }) 
+        })
         reject("Pick an exercise!")      //Returns error if none
     })
 }
@@ -43,8 +43,8 @@ async function minuteButtonClick() {
         radio === 'radio1' ? (exercise = 'Sit Ups', minute, caloriesPer = 10) : //First Button?
             radio === 'radio2' ? (exercise = 'Push Ups', minute, caloriesPer = 15) : //Second Button?
                 (exercise = 'Jump Rope', minute, caloriesPer = 18)              //Third Button!
-        
-        workoutArray.push(new Workout(exercise, minute, caloriesPer * minute)) 
+
+        workoutArray.push(new Workout(exercise, minute, caloriesPer * minute))
         console.log(workoutArray) //For Debugging
 
         //Radio & Text-Box Reset
@@ -56,21 +56,23 @@ async function minuteButtonClick() {
 
 //Most-Burned Button Clicked
 function mostBurnedButtonClick() {
-    let mostBurned = 0;
-    let winningIndex = 0;
-    for (i = 0; i < workoutArray.length; i++) {
-        if (workoutArray[i].calories > mostBurned) {
-            winningIndex = i;
+    try {
+        let mostBurned = 0;
+        let winningIndex = 0;
+        for (i = 0; i < workoutArray.length; i++) {
+            if (workoutArray[i].calories > mostBurned) {
+                winningIndex = i;
+            }
         }
-    }
-    document.getElementById("calorieOutput").value = workoutArray[winningIndex].exercise
+        document.getElementById("calorieOutput").value = workoutArray[winningIndex].exercise + " " + workoutArray[winningIndex].calories
+    } catch { alert("Error!! No exercise information has been entered! ") }
 }
 
 //Show-All Button Clicked
 function showAllButtonClick() {
     let theList = document.getElementById("showAllOutput");
     theList.innerHTML = " ";
-    workoutArray.forEach(function (element, i) {
+    workoutArray.forEach(function (element) {
         var list = document.createElement('li');
         list.innerHTML = "You did " + element.exercise + " for " + element.minutes + " minutes and burned " + element.calories + " calories!";
         theList.appendChild(list);
